79db56d748d91c28a06ae3c7f2cd2410b4c019df
Олександр Черніченко <alex@MacBook-Air-Oleksandr.local>
2020-02-14 13:53:31 +0200

    Updates

---
+++ precache-manifest.3970d016f14666e7ac4d6f0248253c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b8038c6e87ee3604676542650b4850",
+    "url": "/filter-app/index.html"
+  },
+  {
+    "revision": "43d24825803d4e5c7a06",
+    "url": "/filter-app/static/css/main.c6ef7fed.chunk.css"
+  },
+  {
+    "revision": "82908f6fa5748c390507",
+    "url": "/filter-app/static/js/2.53dad8d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/filter-app/static/js/2.53dad8d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43d24825803d4e5c7a06",
+    "url": "/filter-app/static/js/main.0841c413.chunk.js"
+  },
+  {
+    "revision": "82afebad3f904bba3a4e",
+    "url": "/filter-app/static/js/runtime-main.ea0aa6fb.js"
+  }
+]);
